c8c679b71e0248cc40c5af876903d1355552480b
Jonatan Missora <jonatanjmissora@gmail.com>
2026-01-13 13:21:04 -0300

    probamos con options nuevas en routet.tsx para precargar, y lanzar el loading en 0ms. Cargamos datos desde el query en vez del loader

--- src/routes/fake-api/index.tsx
+++ src/routes/fake-api/index.tsx
@@ -1,16 +1,23 @@
-import { getProducts } from '@/server/products'
-import { createFileRoute } from '@tanstack/react-router'
+import { getProducts } from "@/server/products"
+import { useQuery } from "@tanstack/react-query"
+import { createFileRoute } from "@tanstack/react-router"
 
-export const Route = createFileRoute('/fake-api/')({
-  component: RouteComponent,
-  loader: async() => {
-    const response = await getProducts()
-    return response
-  },
-  pendingComponent: () => <div>Loading...</div>
+export const Route = createFileRoute("/fake-api/")({
+	component: RouteComponent,
+	// loader: async() => {
+	//   const response = await getProducts()
+	//   return response
+	// },
+	// pendingComponent: () => <div>Loading...</div>
 })
 
 function RouteComponent() {
-  const response = Route.useLoaderData()
-  return <div>Hello "/fake-api/"! {JSON.stringify(response)}</div>
+	// const response = Route.useLoaderData()
+	const { data, isLoading } = useQuery({
+		queryKey: ["products"],
+		queryFn: getProducts,
+	})
+
+	if (isLoading) return <div>Loading...</div>
+	return <div>Hello "/fake-api/"! {JSON.stringify(data)}</div>
 }
